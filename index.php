431cdc83be1dd21c6e3c9d4d0bfc595eb4448795
unknown <fitra.ramadhan5748@student.unri.ac.id>
2023-06-08 15:09:58 +0700

    Tugas php1

---
+++ index.php
@@ -0,0 +1,26 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/style/phpcss.css">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo'<h1 class="judul">BIODATA</h1>
+        <br>
+        <img src="assets/img/3x4.jpg" alt="">
+        <br><br>
+        <p><strong></strong>Fitra Ramadhan</p>
+        <p>Pekanbaru, 25-11-2001</p>
+        <p>jl.Bayur VI Pandau Permai</p>
+        <p>SMAN 14 Pekanbaru</p>
+        <p>Basket</p>';
+        
+        
+        
+    ?>
+
+</body>
+</html>
